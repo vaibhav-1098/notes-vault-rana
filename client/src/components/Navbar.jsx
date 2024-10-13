006f35d80a5f2ca721e73aa860a57ad3d343129c
vaibhav-1098 <vaibhav.2003.rana@gmail.com>
2024-10-13 20:34:12 +0530

    _

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux"; 
+import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { authActions } from "../redux/store";
@@ -23,60 +23,50 @@ const Navbar = () => {
     };
 
     return (
-        <div>
-            <nav className="flex justify-between items-center bg-yellow-400 p-4 px-5">
-                <div>
-                    <Link className="text-xl font-semibold" to="/">
-                        NotesVault
-                    </Link>
-                </div>
-                <div className="flex gap-6">
-                    {!isLogin && (
-                        <div className="flex justify-center items-center gap-3">
-                            <NavLink
-                                to="/register"
-                                className={({ isActive }) =>
-                                    `p-1 px-2 rounded-md border border-black ${
-                                        isActive ? "bg-amber-500" : "bg-transparent"
-                                    }`
-                                }
-                            >
-                                Register
-                            </NavLink>
-                            <NavLink
-                                to="/login"
-                                className={({ isActive }) =>
-                                    `p-1 px-2 rounded-md border border-black w-16 text-center ${
-                                        isActive ? "bg-amber-500" : "bg-transparent"
-                                    }`
-                                }
-                            >
-                                Login
-                            </NavLink>
+        <nav className="flex justify-between items-center bg-yellow-400 p-4 px-5">
+            <div>
+                <Link className="text-xl font-semibold" to="/">
+                    NotesVault
+                </Link>
+            </div>
+            <div className="flex gap-6">
+                {!isLogin && (
+                    <div className="flex justify-center items-center gap-3">
+                        <NavLink
+                            to="/register"
+                            className={({ isActive }) =>
+                                `p-1 px-2 rounded-md border border-black ${
+                                    isActive ? "bg-amber-500" : "bg-transparent"
+                                }`
+                            }
+                        >
+                            Register
+                        </NavLink>
+                        <NavLink
+                            to="/login"
+                            className={({ isActive }) =>
+                                `p-1 px-2 rounded-md border border-black w-16 text-center ${
+                                    isActive ? "bg-amber-500" : "bg-transparent"
+                                }`
+                            }
+                        >
+                            Login
+                        </NavLink>
+                    </div>
+                )}
+                {isLogin && (
+                    <div className="flex justify-center items-center gap-3 sm:gap-6">
+                        <div className="sm:block hidden">
+                            <i className="bi bi-person-circle"></i>{" "}
+                            <span className="text-sm">{Email}</span>
                         </div>
-                    )}
-                    {isLogin && (
-                        <div className="flex justify-center items-center gap-3 sm:gap-6">
-                            <div className="sm:block hidden">
-                                <i className="bi bi-person-circle"></i>{" "}
-                                <span className="text-sm">{Email}</span>
-                            </div>
-                            <button
-                                onClick={handleLogout}
-                                className="bg-amber-500 p-1 px-2 rounded-md"
-                            >
-                                Logout
-                            </button>
-                        </div>
-                    )}
-                </div>
-            </nav>
-            <a href="/app-release.apk" download>
-                <button className="bg-emerald-500 text-white py-1 w-full text-centre hover:bg-emerald-600 sm:hidden">
-                    Download App
-                </button>
-            </a>
-        </div>
+                        <button onClick={handleLogout} className="bg-amber-500 p-1 px-2 rounded-md">
+                            Logout
+                        </button>
+                    </div>
+                )}
+            </div>
+        </nav>
     );
 };
 
