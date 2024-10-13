ad699ab769f7bf41f176428eae3de8fe2f9d4ad3
vaibhav-1098 <vaibhav.2003.rana@gmail.com>
2024-10-13 20:07:35 +0530

    _

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { authActions } from "../redux/store";
 
 const Navbar = () => {
-    const isLogin =  useSelector((state) => state.isLogin);
+    const isLogin = useSelector((state) => state.isLogin);
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -23,49 +23,60 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="flex justify-between items-center bg-yellow-400 p-4 px-5">
-            <div>
-                <Link className="text-xl font-semibold" to="/">
-                    NotesVault
-                </Link>
-            </div>
-            <div className="flex gap-6">
-                {!isLogin && (
-                    <div className="flex justify-center items-center gap-3">
-                        <NavLink
-                            to="/register"
-                            className={({ isActive }) =>
-                                `p-1 px-2 rounded-md border border-black ${
-                                    isActive ? "bg-amber-500" : "bg-transparent"
-                                }`
-                            }
-                        >
-                            Register
-                        </NavLink>
-                        <NavLink
-                            to="/login"
-                            className={({ isActive }) =>
-                                `p-1 px-2 rounded-md border border-black w-16 text-center ${
-                                    isActive ? "bg-amber-500" : "bg-transparent"
-                                }`
-                            }
-                        >
-                            Login
-                        </NavLink>
-                    </div>
-                )}
-                {isLogin && (
-                    <div className="flex justify-center items-center gap-3 sm:gap-6">
-                        <div className="sm:block hidden">
-                            <i className="bi bi-person-circle"></i> <span className="text-sm">{Email}</span>
+        <div>
+            <nav className="flex justify-between items-center bg-yellow-400 p-4 px-5">
+                <div>
+                    <Link className="text-xl font-semibold" to="/">
+                        NotesVault
+                    </Link>
+                </div>
+                <div className="flex gap-6">
+                    {!isLogin && (
+                        <div className="flex justify-center items-center gap-3">
+                            <NavLink
+                                to="/register"
+                                className={({ isActive }) =>
+                                    `p-1 px-2 rounded-md border border-black ${
+                                        isActive ? "bg-amber-500" : "bg-transparent"
+                                    }`
+                                }
+                            >
+                                Register
+                            </NavLink>
+                            <NavLink
+                                to="/login"
+                                className={({ isActive }) =>
+                                    `p-1 px-2 rounded-md border border-black w-16 text-center ${
+                                        isActive ? "bg-amber-500" : "bg-transparent"
+                                    }`
+                                }
+                            >
+                                Login
+                            </NavLink>
                         </div>
-                        <button onClick={handleLogout} className="bg-amber-500 p-1 px-2 rounded-md">
-                            Logout
-                        </button>
-                    </div>
-                )}
-            </div>
-        </nav>
+                    )}
+                    {isLogin && (
+                        <div className="flex justify-center items-center gap-3 sm:gap-6">
+                            <div className="sm:block hidden">
+                                <i className="bi bi-person-circle"></i>{" "}
+                                <span className="text-sm">{Email}</span>
+                            </div>
+                            <button
+                                onClick={handleLogout}
+                                className="bg-amber-500 p-1 px-2 rounded-md"
+                            >
+                                Logout
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </nav>
+            <a href="/app-release.apk" download>
+                <button className="bg-emerald-500 text-white py-1 w-full text-centre hover:bg-emerald-600 sm:hidden">
+                    Download App
+                </button>
+            </a>
+        </div>
     );
 };
 
